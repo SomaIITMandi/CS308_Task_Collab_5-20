cf78b6891fa9e1421073ce95d78bc8bf727d9b98
Soma <s17008@students.iitmandi.co.in>
2020-10-05 12:50:16 +0530

    Initial commit of utility functions

---
+++ utility_fns.py
@@ -0,0 +1,29 @@
+#CS308 : Task - colaboration1
+#Some utility functions
+#Ocotober 5, 2020 
+
+import numpy as np
+
+#min-max function
+def max_min(data):
+  l = data[0]
+  u = data[0]
+  for num in data:
+    if num< l:
+      l = num
+    elif num> u:
+        u = num
+  return l, u
+
+#add two arrays
+
+#average an array
+
+
+#Input array
+a = [0, 10, 15, 40, -5, 42, 17, 28, 75]
+
+#output
+min, max = np.array(max_min(a))
+
+print('Maximum value = {:.2f}, minimum value = {:.2f}'.format(max, min))
